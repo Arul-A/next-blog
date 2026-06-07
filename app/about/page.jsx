e4e340156e1e366c43bc7bff9453ccc7969bd4a8
Arul-A <akarul2100@gmail.com>
2026-06-07 20:37:21 +0530

    initial commit

---
+++ app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <main className="container mx-auto px-4 py-6">
+            <h2 className="text-4xl font-bold mb-4">About Us</h2>
+            <p>Our blog platform is built to share ideas, stories, and knowledge in a simple and user-friendly way.</p>
+        </main>
+    )
+}
